62d0b4c4dfed3ad5c811f786e6f6af6ff860154a
afksashka <afksashka@gmail.com>
2022-05-10 13:18:38 +0700

    Add EntityBase

---
+++ NewProject/Domain/Entities/EntityBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewProject.Domain.Entities
+{
+    public abstract class EntityBase
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Display(Name ="Название (заголовок)")]
+        public virtual string Title { get; set; }
+
+        [Display(Name = "Краткое описание")]
+        public virtual string Subtitle { get; set; }
+
+        [Display(Name = "Полное описание")]
+        public virtual string Text { get; set; }
+
+        [Display(Name = "SEO мететаег Title")]
+        public virtual string MetaTitle { get; set; }
+
+        [Display(Name = "SEO мететаег Description")]
+        public virtual string MetaDescription { get; set; }
+
+        [Display(Name = "SEO мететаег Keywords")]
+        public virtual string MetaKeywords { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime DateAdded { get; set; }
+
+    }
+}
